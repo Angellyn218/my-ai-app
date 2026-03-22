9b4be2a63e482cf0f60f82148b955286d00616eb
Angellyn218 <angel.lyn.cervantes@gmail.com>
2026-03-22 13:54:03 -0700

    added initial message and user input components with initial main page design

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Message = ( { text, speaker } ) => {
+    return (
+        <div className="mx-auto w-full flex flex-col justify-center items-center sm:justify-start sm:items-start">
+            <label htmlFor='message' className="block text-sm/6 font-medium text-gray-900 sm:justify-start">{speaker}</label>
+            <p className='sm:px-4'>{text}</p>
+        </div>
+    )
+}
+
+export default Message;
